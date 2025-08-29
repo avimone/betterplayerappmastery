a3015b8c609475ae0ddf361e7470bc88ecac5c6d
avimone <40avirajpatel@gmail.com>
2025-08-29 23:25:16 +0530

    fix absolute Time

--- android/src/main/kotlin/com/jhomlala/better_player/BetterPlayerPlugin.kt
+++ android/src/main/kotlin/com/jhomlala/better_player/BetterPlayerPlugin.kt
@@ -434,8 +434,7 @@ private fun enablePictureInPicture(player: BetterPlayer) {
             
             // Enable auto-enter PiP on user leave hint for Android 12+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                val autoEnterEnabled = getParameter(parameters, "autoEnterOnUserLeaveHint", false)
-                 pipParamsBuilder.setAutoEnterEnabled(autoEnterEnabled)
+                pipParamsBuilder.setAutoEnterEnabled(false)
             }
             
             // Support seamless resize for better transition from fullscreen
